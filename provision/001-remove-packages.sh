b202c3e59bcb68d7b29f06670d78fc8308f5a379
Ville Korhonen <ville@xd.fi>
2015-02-23 12:51:50 +0200

    Drop reference to Linux 3.14.

--- provision/001-remove-packages.sh
+++ provision/001-remove-packages.sh
@@ -4,4 +4,4 @@ set -e
 
 export DEBIAN_FRONTEND="noninteractive"
 
-apt-get -y remove avahi-autoipd bluetooth linux-image-3.14-* netcat-traditional pinentry-gtk2 reportbug rpcbind telnet w3m wamerican wireless-tools wpasupplicant ftp geoip-database hicolor-icon-theme iamerican ibritish ienglish-common libavahi-* nfs-common task-english wireless-regdb
+apt-get -y remove avahi-autoipd bluetooth netcat-traditional pinentry-gtk2 reportbug rpcbind telnet w3m wamerican wireless-tools wpasupplicant ftp geoip-database hicolor-icon-theme iamerican ibritish ienglish-common libavahi-* nfs-common task-english wireless-regdb
