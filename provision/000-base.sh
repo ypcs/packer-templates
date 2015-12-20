693a5893ed6801c9eee61180e6d20cb4aa107a38
Ville Korhonen <ville@xd.fi>
2015-12-20 17:03:02 +0200

    Drop obsolete network rules

--- provision/000-base.sh
+++ provision/000-base.sh
@@ -39,12 +39,6 @@ EOF
 # Removing leftover DHCP leases
 rm /var/lib/dhcp/*.leases
 
-# Deactivate name persistence for network interfaces
-#dpkg-divert --divert /lib/udev/write_net_rules \
-#            --rename /lib/udev/write_net_rules.udev
-#cp /bin/true /lib/udev/write_net_rules
-rm -f /etc/udev/rules.d/70-persistent-net.rules
-
 # Adding a 2 sec delay to the interface up, to make the dhclient happy
 echo "pre-up sleep 5" >> /etc/network/interfaces
 
